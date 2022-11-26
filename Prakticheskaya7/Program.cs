2071e800b8e8e9ff7d791e9abdec16ba432fdd70
memberRemember <94972595+memberRemember@users.noreply.github.com>
2022-11-26 07:30:05 +0300

    Update Program.cs

--- Prakticheskaya7/Program.cs
+++ Prakticheskaya7/Program.cs
@@ -6,6 +6,6 @@ internal class Program
     private static void Main(string[] args)
     {
         Functions Functions = new Functions();
-        Functions.SolnceEsheNeSelo();
+        Functions.SolnceEsheVisoko();
     }
 }
